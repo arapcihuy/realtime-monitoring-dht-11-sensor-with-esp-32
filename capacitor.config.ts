d5ee589c3a303d8a12d20ead55de5a6043c8ca2a
arapcihuy <21523182@students.uii.ac.id>
2024-12-18 18:51:54 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Realtime Monitoring DHT 11 Sensor with ESP32',
+  webDir: 'www'
+};
+
+export default config;
